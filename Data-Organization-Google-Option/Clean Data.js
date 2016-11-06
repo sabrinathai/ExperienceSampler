729550430dd96695d4fc18c90711d07eb28430ee
sabrinathai <sabrina.thai@utoronto.ca>
2016-11-06 12:29:06 -0500

    Updated Clean Data.js

--- Data-Organization-Google-Option/Clean Data.js
+++ Data-Organization-Google-Option/Clean Data.js
@@ -1,9 +1,11 @@
 function cleanData() {
   var totalSheets = SpreadsheetApp.getActiveSpreadsheet().getNumSheets();
-  for (i in totalSheets) {
-    var sheet = SpreadsheetApp.getActiveSheet()[i];
+  for (var i=0; i< totalSheets; i++) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[i];
+    Logger.log(sheet);
     removeDuplicates(sheet);
     removeEmptyRows(sheet);
+    Logger.log("I ran the for function");
   }
 }
 
