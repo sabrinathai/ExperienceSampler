9a53e999c9c0d32d29b87bb5e9461a93dea94fc0
sabrinathai <sabrina.thai@utoronto.ca>
2016-11-06 21:58:58 -0500

    Update Convert Data.R

--- Data-Organization-R-Option/Convert Data.R
+++ Data-Organization-R-Option/Convert Data.R
@@ -6,10 +6,8 @@ library("stringr")
 library("plyr")
 
 ####READ IN THIS FILE TO TEST LONG FORM CONVERSION####
+setwd("") #set directory to where the file with all the merged spliced data is. 
 spliced.dataframe <- read.csv("all.spliced.data.csv", header=T)
-setwd("/Users/sabrinathai/Documents/Research/Outside Project/Daily Relationship Comparisons Data")
-spliced.dataframe <- read.csv("CRC All Spliced Data with Corrected Variable Names and Corrected Unique Keys - USE THIS TO CONVERT TO LONG FORM - ONE COMPARISON PER LINE.csv", header=T)
-
 
 ####CONVERT DATA TO LONG FORM####
 ####PREPARATION####
@@ -23,34 +21,40 @@ View(unique.variable.names)
 ###Check variable names to see if any numbers or symbols (e.g., "-") have been appended to them. We will remove these symbols now. 
 ###This ensures that all values for the same variable will be written to the same column in the data frame.  
 ##find and replace all variable names that were converted weird
+#the first argument is the original variable name that is incorrect
+#the second argument is the corrected variable name
+#the third argument is the column
+#below are some examples
 unique.variable.names$spliced.dataframe.variable.name<-gsub("activity1","activity", unique.variable.names$spliced.dataframe.variable.name)
 unique.variable.names$spliced.dataframe.variable.name<-gsub("domain1","domain", unique.variable.names$spliced.dataframe.variable.name)
 unique.variable.names$spliced.dataframe.variable.name<-gsub("-","", unique.variable.names$spliced.dataframe.variable.name)
 ###You can use the line below to remove any variables that are not actual variables###
 #enter the number of the row you want to delete
 #if you have multiple rows to delete use "-c(X, Y, Z,...), where X, Y, and Z are the row numbers
-unique.variable.names<-unique.variable.names[-53,]
+unique.variable.names<-unique.variable.names[-X,]
+#Check the variable names again
 View(unique.variable.names)
 ##Now we remove any duplicates that appeared because of strange symbols
 unique.variable.names <- unique(unique.variable.names, )
 row.names(unique.variable.names)<-NULL
 colnames(unique.variable.names)<-NULL
+#Check the variable names again
 View(unique.variable.names)
 unique.variables.list<-as.list(unique.variable.names)
 unique.variable.names.unlist<-unlist(unique.variable.names)
 complete.data<-c("id","start.time","end.time", unique.variable.names.unlist)
 
-####Find and Replace All Variable Names That Were Converted Weird in the ACTUAL Dataset####
+####Find and Replace All Variable Names That Were Converted Weird in the ACTUAL Spliced Dataset####
+###These should match the lines above, starting from line 28
 spliced.dataframe$variable.name<-gsub("activity1","activity", spliced.dataframe$variable.name)
 spliced.dataframe$variable.name<-gsub("domain1","domain", spliced.dataframe$variable.name)
 spliced.dataframe$variable.name<-gsub("-","", spliced.dataframe$variable.name)
-write.csv(spliced.dataframe, "all.spliced.data.fixed.csv", row.names=FALSE)
 
 ####Save the Fixed Dataset####
-setwd("/Users/sabrinathai/Documents/Clean and Convert ExperienceSampler Server Data/No Loops in Data/Spliced Test Data")
-data.to.convert <- read.csv("all.spliced.data.fixed.csv", header=TRUE)
-data.to.convert <- read.csv("CRC All Spliced Data with Corrected Variable Names and Corrected Unique Keys - USE THIS TO CONVERT TO LONG FORM - ONE COMPARISON PER LINE.csv", header=TRUE, stringsAsFactors=F)
+write.csv(spliced.dataframe, "all.spliced.data.fixed.csv", row.names=FALSE)
 
+####Read in the Fixed Spliced Dataset to convert####
+data.to.convert <- read.csv("all.spliced.data.fixed.csv", header=TRUE)
 
 ####CONVERSION####
 #Handy function to strip prefixes from the column names (of cosmetic value only!) since the reshape process will add a prefix:
@@ -83,27 +87,29 @@ restructured.data <- merge(restructured.data, end.times, by = c("id","unique.key
 colnames(restructured.data) <- colnames_removing_prefix(restructured.data, "values.")
 View(restructured.data)
 #reorder the columns (optional)
-reorganized.data <- restructured.data[ ,c("id", "unique.key","start.time","end.time","socialComparison","snooze","context", "activity","otherActivity",
-                                       "timeSinceComparison","target","imaginaryPerson","celebrity","otherTarget","natureOfComparison","domain",
-                                       "domainImportance","partnerDomainImportance","contact","activeComparison","comparisonMood",
-                                       "comparisonOptimism","comparisonSatisfaction","comparisonFeelingsAboutPartner","comparisonFeelingsAboutSelf",
-                                       "intensity","moreComparisons","timeSinceInteraction","interactionLength","modeOfSocialInteraction",
-                                       "soughtSocialSupport","helpfulnessOfOwnSupport","helpfulnessOfOtherSupport","socialInteractionConflict",
-                                       "conflictInstigator","conflictIntensity", "conflictResolution",
-                                       "interactionAnxious","interactionAngry","interactionHappy","interactionRelaxed","interactionThoughts",
-                                       "interactionMemories","interactionInitiator","interactionEnder","closenessToInteractionPartner",
-                                       "interactionPartnerAge","interactionPartnerEthnicity","interactionPartnerSex",
-                                       "interactionPartnerSexualOrientation", "sameInteraction","mood", "optimism", "satisfaction",
-                                       "feelingsAboutPartner","feelingsAboutSelf")]
+#uncomment if you want to reorder. You need to input the variable names in the order you wish.
+#Each variable name must be in quotations. 
+# reorganized.data <- restructured.data[ ,c("id", "unique.key","start.time","end.time","","","","","",
+#                                        "","","","","","","","","","",
+#                                        "","","","","","","","","","",
+#                                        "","","","","","","","","","",
+#                                        "","","","","","","","","","",
+#                                        "","","","","","","","","","",
+#                                        "","","","","","","","","","",
+#                                        "","","","","","","","","","",
+#                                        "","","","","","","","","","",
+#                                        "","","","","","","","","","",
+#                                        "","","","","","","","","","",
+#                                        "","","","","","","","","","")]
 
 #check your data frame to determine which variables to sort by                                       
 View(reorganized.data)
-#organize data by id and uniqu key
+#organize data by id and unique key
 reorganized.data <- reorganized.data[order(reorganized.data[,1], reorganized.data[,2]),]
 row.names(reorganized.data)<-NULL
 #check your data frame before saving it
 View(reorganized.data)
 
 #write as a csv file
-setwd("/Users/sabrinathai/Documents/Clean and Convert ExperienceSampler Server Data/No Loops in Data")
-write.csv(reorganized.data, "CRC Experience Sampling Data - One Comparison Per Line.csv", row.names=FALSE)
+setwd("") #set directory to where you want to save your long form data
+write.csv(reorganized.data, "ExperienceSampler Long Form Data.csv", row.names=FALSE)
