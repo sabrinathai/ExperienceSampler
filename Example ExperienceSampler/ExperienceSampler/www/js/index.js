12e675cbccc4b5d7c0f840ed6a71dbfb578a2318
Sabrina Thai <sabrina.thai@utoronto.ca>
2017-10-12 20:33:27 -0400

    Update index.js

--- Example ExperienceSampler/ExperienceSampler/www/js/index.js
+++ Example ExperienceSampler/ExperienceSampler/www/js/index.js
@@ -258,7 +258,7 @@ var participantSetup = [
 var NUMSETUPQS = participantSetup.length;
 var SNOOZEQ = 0;
 var questionTmpl = "<p>{{{questionText}}}</p><ul>{{{buttons}}}</ul>";
-var questionTextTmpl = "{{questionPrompt}}";
+var questionTextTmpl = "{{{questionPrompt}}}";
 var buttonTmpl = "<li><button id='{{id}}' value='{{value}}'>{{label}}</button></li>";
 var textTmpl = "<li><textarea cols=50 rows=5 id='{{id}}'></textarea></li><li><button type='submit' value='Enter'>Enter</button></li>";
 var checkListTmpl =  "<li><input type='checkbox' id='{{id}}' value='{{value}}'>{{label}}</input></li>";
